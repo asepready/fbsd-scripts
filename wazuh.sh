93c952bd631ce439baf7481ff5ebf6ac942bdb49
Asep Ready <asepdevops@gmail.com>
2025-05-18 00:41:00 +0700

    wazuh

--- wazuh.sh
+++ wazuh.sh
@@ -1,17 +1,11 @@
-JAIL_IP=127.0.0.1
-# install required packages
-pkg install -y wazuh-indexer
+pkg install -y wazuh-dashboard wazuh-indexer wazuh-server
 
-# copy files
-cp wazuh/etc /
-cp wazuh/usr /
-
-sed -e "s,%%SERVER_IP%%,$JAIL_IP,g" -i "" /usr/local/etc/opensearch/opensearch.yml
+# Genarate certificates
 cd /root/; fetch "https://people.freebsd.org/~acm/ports/wazuh/wazuh-gen-certs.tar.gz"
 cd /root/; tar xvfz wazuh-gen-certs.tar.gz
-echo 'dashboard_ip="'$JAIL_IP'"' > /root/wazuh-gen-certs/dashboard.lst
-echo 'indexer1_ip="'$JAIL_IP'"' > /root/wazuh-gen-certs/indexer.lst
-echo 'server1_ip="'$JAIL_IP'"' > /root/wazuh-gen-certs/server.lst
+echo 'dashboard_ip=127.0.0.1' > /root/wazuh-gen-certs/dashboard.lst
+echo 'indexer1_ip=127.0.0.1' > /root/wazuh-gen-certs/indexer.lst
+echo 'server1_ip=127.0.0.1' > /root/wazuh-gen-certs/server.lst
 cd /root/wazuh-gen-certs; echo y | sh gen-certs.sh
 
 mkdir -p /usr/local/etc/opensearch/certs
@@ -23,11 +17,18 @@ cp /root/wazuh-gen-certs/wazuh-certificates/root-ca.pem /usr/local/etc/opensearc
 chmod -R 640 /usr/local/etc/opensearch/certs
 chown -R opensearch:opensearch /usr/local/etc/opensearch/certs
 
+cp /usr/local/etc/wazuh-indexer/wazuh-indexer.yml /usr/local/etc/opensearch/opensearch.yml
+sed -e "s,network.host: "10.0.0.10",network.host: "127.0.0.1",g" -i "" /usr/local/etc/opensearch/opensearch.yml
+
 cd /usr/local/etc/opensearch/opensearch-security; sh -c 'for i in $(ls *.sample ) ; do cp -p ${i} $(echo ${i} | sed "s|.sample||g") ; done'
 cd /usr/local/lib/opensearch/plugins/opensearch-security/tools; sh -c "OPENSEARCH_JAVA_HOME=/usr/local/openjdk${v_jdk} ./hash.sh -p adminpass"
 
 # enable and start service
+SYSRC wazuh_manager_enable=YES
+SYSRC filebeat_enable=YES
+SYSRC logstash_enable=YES
 SYSRC opensearch_enable=YES
-SYSRC opensearch_java_home=/usr/local/openjdk${v_jdk}
+SYSRC opensearch_dashboards_enable=YES
+SYSRC opensearch_dashboards_syslog_output_enable=YES
 
 SERVICE opensearch start
